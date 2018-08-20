ce58af12a85371e44aef71799d6cabb86227092d
Frankcando <diablof@gmail.com>
2018-08-20 21:01:43 +0800

    first

---
+++ analysis_record.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+' a test module '
+__author__ = 'Frank yu'
+
+# #这个代码是为了根据交易记录统计分析交易的各项数据，比如 最大回撤，年化收益率，夏普率等等
+
+
+import pandas as pd
+import os
+import requests
+
+from datetime import datetime
+
