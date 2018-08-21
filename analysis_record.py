4cd64e38502877477db6b74f434f9d2e7d04f060
Frankcando <diablof@gmail.com>
2018-08-21 10:57:42 +0800

    add read code

--- analysis_record.py
+++ analysis_record.py
@@ -6,6 +6,7 @@ __author__ = 'Frank yu'
 # #这个代码是为了根据交易记录统计分析交易的各项数据，比如 最大回撤，年化收益率，夏普率等等
 
 import pandas as pd
+import numpy as np
 import os
 import requests
 import csv
@@ -14,11 +15,16 @@ from datetime import datetime
 
 print("begin to analysis trading records")
 
-FileName = 'D:\\GoPath\src\\re_doc\\1534239306_tradedetail.csv'
-with open(FileName,'r', encoding='UTF-8') as file_obj:
-    lines = file_obj.readlines()
-    for line in lines:
-        print(line)
+FileName = '1534239306_tradedetail.csv'
+f = open(FileName,'r' , encoding= 'utf-8')
+df_detail = pd.read_csv(f)
+print(str(df_detail))
+
+FileName = 'btcusd_day_day.csv'
+f = open(FileName, 'r', encoding='utf-8')
+df_day = pd.read_csv(f)
+print(str(df_day))
+
 
 
 print("analysis end")
