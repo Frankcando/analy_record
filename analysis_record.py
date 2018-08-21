83f51f388a10f09abf5894fc5101341e36dca022
Frankcando <diablof@gmail.com>
2018-08-21 17:22:51 +0800

    read data by pd is ok

--- analysis_record.py
+++ analysis_record.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-' a test module '
-__author__ = 'Frank yu'
+__author__ = 'Frank'
 
-# #这个代码是为了根据交易记录统计分析交易的各项数据，比如 最大回撤，年化收益率，夏普率等等
+# #this code is to analysis
 
 import pandas as pd
+from pandas import DataFrame, Series
 import numpy as np
 import os
 import requests
@@ -13,18 +13,35 @@ import csv
 
 from datetime import datetime
 
-print("begin to analysis trading records")
+global File_detail, File_day
 
-FileName = '1534239306_tradedetail.csv'
-f = open(FileName,'r' , encoding= 'utf-8')
-df_detail = pd.read_csv(f)
-print(str(df_detail))
+File_detail = '1534239306_tradedetail.csv'
+File_day = 'day_day.csv'
 
-FileName = 'btcusd_day_day.csv'
-f = open(FileName, 'r', encoding='utf-8')
-df_day = pd.read_csv(f)
-print(str(df_day))
+def read_data():
+    print("begin to read data")
+
+    try:
+        with open(File_detail) as obj_detail:
+            print('nothing, only test')
+
+        with open(File_day) as obj_day:
+            print('nothing,only test')
+
+    except FileNotFoundError:
+        print("file no exist")
+    else:
+        print("begin to read data")
+        df_detail = pd.read_csv(File_detail, encoding='utf-8')
+        df_day = pd.read_csv(File_day,encoding='utf-8')
+        #print(df_day[0:3])
+        print("read end")
+
+if __name__ == "__main__":
+    read_data()
 
 
 
-print("analysis end")
+
+
+
